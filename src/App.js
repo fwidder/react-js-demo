5d4d7d176ef8a2c8840f40094c3f8b14bec91af4
Florian Widder <florian.widder@live.de>
2018-04-26 11:42:11 +1200

    Added content and functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button, Input, Row } from 'reactstrap';
 import './App.css';
+import dataInit from './demodata.json';
+import JsonTable from 'ts-react-json-table';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.state = {
+      data: dataInit,
+      inputText: '',
+      col: [
+        { key: 'sender', label: 'Sender:' },
+        {
+          key: 'reciever',
+          label: 'Reciever:'
+        },
+        {
+          key: 'message',
+          label: 'Message:'
+        }
+      ]
+    };
+  }
+
+  sendMessage() {
+    const tmp = this.state.data;
+    tmp.push({
+      sender: 'ONE',
+      reciever: 'TWO',
+      message: this.state.inputText
+    });
+    this.setState({ data: tmp });
+    this.setState({ inputText: '' });
+  }
+
+  updateInputValue(evt) {
+    this.setState({
+      inputText: evt.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Row>
+          <h1>Demo Page</h1>
+        </Row>
+        <Row>
+          <JsonTable
+            className="App"
+            caption="Messages:"
+            rows={this.state.data}
+            columns={this.state.col}
+          />
+        </Row>
+        <Row>
+          <Input
+            placeholder="Type here!"
+            value={this.state.inputText}
+            onChange={evt => this.updateInputValue(evt)}
+          />
+          <Button color="primary" onClick={this.sendMessage}>
+            Send
+          </Button>
+        </Row>
       </div>
     );
   }
